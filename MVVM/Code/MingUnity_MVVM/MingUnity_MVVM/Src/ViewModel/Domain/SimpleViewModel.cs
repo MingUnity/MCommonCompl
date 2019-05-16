e1bfe5ed344126120b0ba2538a0d46029fc1c446
陈铭 <950902@nd.com>
2019-05-16 15:16:18 +0800

    <mvvm> 1.add SimpleViewModel

---
+++ MVVM/Code/MingUnity_MVVM/MingUnity_MVVM/Src/ViewModel/Domain/SimpleViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MingUnity.MVVM.Model;
+
+namespace MingUnity.MVVM.ViewModel
+{
+    /// <summary>
+    /// 简单视图模型
+    /// </summary>
+    public sealed class SimpleViewModel : ViewModelBase<IModel>
+    {
+        /// <summary>
+        /// 激活
+        /// </summary>
+        public override bool Active
+        {
+            get
+            {
+                return _active;
+            }
+            set
+            {
+                _active = value;
+
+                RaisePropertyChanged("Active");
+            }
+        }
+
+        /// <summary>
+        /// 装载
+        /// </summary>
+        public override void Setup()
+        {
+            Active = _active;
+        }
+    }
+}
--- MVVM/Code/MingUnity_MVVM/MingUnity_MVVM/Src/ViewModel/Domain/ViewModelBase.cs
+++ MVVM/Code/MingUnity_MVVM/MingUnity_MVVM/Src/ViewModel/Domain/ViewModelBase.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using MingUnity.MVVM.Model;
 
 namespace MingUnity.MVVM.ViewModel
