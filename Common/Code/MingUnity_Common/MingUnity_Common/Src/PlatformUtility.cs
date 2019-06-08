fc1caf19fb9af5f536ac3fb0559e52c3e2c1e6f9
Ming <491913833@qq.com>
2019-06-08 18:57:47 +0800

    <common> 1.add PlatformUtility.cs TaskManager.cs

---
+++ Common/Code/MingUnity_Common/MingUnity_Common/Src/PlatformUtility.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace MingUnity.Common
+{
+    /// <summary>
+    /// 平台通用
+    /// </summary>
+    public static class PlatformUtility
+    {
+        /// <summary>
+        /// 获取加载资源时streamingAssets路径
+        /// </summary>
+        public static string GetResStreamingAssets(bool syncLoad = true)
+        {
+            string dir = string.Empty;
+
+            switch (Application.platform)
+            {
+                case RuntimePlatform.Android:
+                    {
+                        dir = syncLoad ? "{0}!assets" : "jar:file://{0}!/assets";
+
+                        dir = string.Format(dir, Application.dataPath);
+                    }
+
+                    break;
+
+                case RuntimePlatform.IPhonePlayer:
+                    {
+                        dir = syncLoad ? "{0}/Raw" : "file://{0}/Raw";
+
+                        dir = string.Format(dir, Application.dataPath);
+                    }
+
+                    break;
+
+                default:
+                    dir = Application.streamingAssetsPath;
+                    break;
+            }
+
+            return dir;
+        }
+    }
+}
