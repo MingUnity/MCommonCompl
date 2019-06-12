484639dd4711e0ff61f21737e7af1cab8547c975
陈铭 <950902@nd.com>
2019-06-12 19:07:15 +0800

    <voiceinput> 1.add voiceinput module

---
+++ VoiceInput/Code/MingUnity_VoiceInput/MingUnity_VoiceInput/Src/Core/VoiceReceiver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace MingUnity.VoiceInput
+{
+    /// <summary>
+    /// 声音接收器
+    /// </summary>
+    public partial class VoiceReceiver
+    {
+        private int _index;
+
+        public VoiceReceiver(int index)
+        {
+            this._index = index;
+        }
+
+        public string Name
+        {
+            get
+            {
+                string res = string.Empty;
+
+                string[] devices = Microphone.devices;
+
+                TryGetValue(devices, _index, ref res);
+
+                return res;
+            }
+        }
+
+        /// <summary>
+        /// 从数组中获取值
+        /// </summary>
+        private void TryGetValue<T>(T[] array, int index, ref T value)
+        {
+            if (IndexValid(array, index))
+            {
+                value = array[index];
+            }
+        }
+
+        /// <summary>
+        /// 数组取值有效性
+        /// </summary>
+        private bool IndexValid(Array array, int index)
+        {
+            bool result = false;
+
+            if (array != null && index >= 0 && index < array.Length)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ VoiceInput/Code/MingUnity_VoiceInput/MingUnity_VoiceInput/Src/Core/VoiceReceiver_static.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MingUnity.VoiceInput
+{
+    /// <summary>
+    /// 声音接收器
+    /// </summary>
+    public partial class VoiceReceiver
+    {
+        /// <summary>
+        /// 声音接收器列表
+        /// </summary>
+        private static Dictionary<int, VoiceReceiver> _voiceReceiverDic = new Dictionary<int, VoiceReceiver>();
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public static void Initialize(Action<bool> callback = null)
+        {
+            if (Application.HasUserAuthorization(UserAuthorization.Microphone))
+            {
+                callback?.Invoke(true);
+            }
+            else
+            {
+                TaskManager.CreateTask(AuthorizeRequest(callback));
+            }
+        }
+
+        /// <summary>
+        /// 麦克数目
+        /// </summary>
+        public static int Count
+        {
+            get
+            {
+                int res = 0;
+
+                string[] devices = Microphone.devices;
+
+                if (devices != null)
+                {
+                    res = devices.Length;
+                }
+
+                return res;
+            }
+        }
+
+        /// <summary>
+        /// 获取声音接收器
+        /// </summary>
+        public static VoiceReceiver Get(int index)
+        {
+            VoiceReceiver result = null;
+
+            _voiceReceiverDic?.TryGetValue(index, out result);
+
+            if (result == null)
+            {
+                result = new VoiceReceiver(index);
+                
+                if (_voiceReceiverDic != null)
+                {
+                    _voiceReceiverDic[index] = result;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 获取声音接收器
+        /// </summary>
+        public static VoiceReceiver Get(string name)
+        {
+            VoiceReceiver result = null;
+
+            for (int i = 0; i < Count; i++)
+            {
+                VoiceReceiver voiceReceiver = Get(i);
+
+                if (voiceReceiver != null && voiceReceiver.Name == name)
+                {
+                    result = voiceReceiver;
+
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 请求授权
+        /// </summary>
+        private static IEnumerator AuthorizeRequest(Action<bool> callback = null)
+        {
+            yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+
+            callback?.Invoke(Application.HasUserAuthorization(UserAuthorization.Microphone));
+        }
+    }
+}
