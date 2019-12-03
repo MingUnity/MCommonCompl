16f7b899a9f369e7cd4d6d186d08ebe9f91b1455
Ming <491913833@qq.com>
2019-12-03 15:59:16 +0800

    <Ming> 1.Add 增加buttonex 和toggleex

---
+++ UIExtends/UIExtends/UIExtends/Src/ButtonEx.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace UnityEngine.UI
+{
+    /// <summary>
+    /// UGUI按钮扩展
+    /// </summary>
+    public class ButtonEx : Button
+    {
+        /// <summary>
+        /// 文本
+        /// </summary>
+        [SerializeField]
+        private Text _text;
+
+        /// <summary>
+        /// 普通态 文本颜色
+        /// </summary>
+        [SerializeField]
+        private Color _normalTextColor = Color.white;
+
+        /// <summary>
+        /// 高亮态 文本颜色
+        /// </summary>
+        [SerializeField]
+        private Color _highlightedTextColor = Color.white;
+
+        /// <summary>
+        /// 点击态 文本颜色
+        /// </summary>
+        [SerializeField]
+        private Color _pressedTextColor = Color.white;
+
+        /// <summary>
+        /// 禁用态 文本颜色
+        /// </summary>
+        [SerializeField]
+        private Color _disabledTextColor = Color.white;
+
+        /// <summary>
+        /// 普通态事件
+        /// </summary>
+        [SerializeField]
+        public UnityEvent onNormal = new UnityEvent();
+
+        /// <summary>
+        /// 高亮态事件
+        /// </summary>
+        [SerializeField]
+        public UnityEvent onHighlighted = new UnityEvent();
+
+        /// <summary>
+        /// 点击态
+        /// </summary>
+        [SerializeField]
+        public UnityEvent onPressed = new UnityEvent();
+
+        /// <summary>
+        /// 禁用态 
+        /// </summary>
+        [SerializeField]
+        public UnityEvent onDisabled = new UnityEvent();
+
+        /// <summary>
+        /// 进入悬浮态
+        /// </summary>
+        [SerializeField]
+        public UnityEvent onHoverEnter = new UnityEvent();
+
+        /// <summary>
+        /// 退出悬浮态
+        /// </summary>
+        [SerializeField]
+        public UnityEvent onHoverExit = new UnityEvent();
+
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            base.DoStateTransition(state, instant);
+
+            switch (state)
+            {
+                case SelectionState.Normal:
+                    if (_text != null)
+                    {
+                        _text.color = _normalTextColor;
+
+                    }
+                    onNormal.Invoke();
+                    break;
+
+                case SelectionState.Highlighted:
+                    if (_text != null)
+                    {
+                        _text.color = _highlightedTextColor;
+                    }
+                    onHighlighted.Invoke();
+                    break;
+
+                case SelectionState.Pressed:
+                    if (_text != null)
+                    {
+                        _text.color = _pressedTextColor;
+                    }
+                    onPressed.Invoke();
+                    break;
+
+                case SelectionState.Disabled:
+                    if (_text != null)
+                    {
+                        _text.color = _disabledTextColor;
+                    }
+                    onDisabled.Invoke();
+                    break;
+            }
+        }
+
+        public override void OnPointerEnter(PointerEventData eventData)
+        {
+            base.OnPointerEnter(eventData);
+
+            onHoverEnter?.Invoke();
+        }
+
+        public override void OnPointerExit(PointerEventData eventData)
+        {
+            base.OnPointerExit(eventData);
+
+            onHoverExit?.Invoke();
+        }
+    }
+
+}
---
+++ UIExtends/UIExtendsEditor/UIExtendsEditor/Src/ToggleExEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine.UI;
+
+namespace UnityEditor.UI
+{
+    [CustomEditor(typeof(ToggleEx), true)]
+    [CanEditMultipleObjects]
+    public class ToggleExEditor : ToggleEditor
+    {
+        private SerializedProperty _text;
+        private SerializedProperty _clrNormal;
+        private SerializedProperty _clrHighlighted;
+        private SerializedProperty _clrPressed;
+        private SerializedProperty _clrDisabled;
+        private SerializedProperty _clrIsOn;
+        private SerializedProperty _onNormal;
+        private SerializedProperty _onHighlighted;
+        private SerializedProperty _onPressed;
+        private SerializedProperty _onDisabled;
+        private SerializedProperty _onHoverEnter;
+        private SerializedProperty _onHoverExit;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _text = serializedObject.FindProperty("_text");
+            _clrNormal = serializedObject.FindProperty("_normalTextColor");
+            _clrHighlighted = serializedObject.FindProperty("_highlightedTextColor");
+            _clrPressed = serializedObject.FindProperty("_pressedTextColor");
+            _clrDisabled = serializedObject.FindProperty("_disabledTextColor");
+            _clrIsOn = serializedObject.FindProperty("_isOnTextColor");
+            _onNormal = serializedObject.FindProperty("onNormal");
+            _onHighlighted = serializedObject.FindProperty("onHighlighted");
+            _onPressed = serializedObject.FindProperty("onPressed");
+            _onDisabled = serializedObject.FindProperty("onDisabled");
+            _onHoverEnter = serializedObject.FindProperty("onHoverEnter");
+            _onHoverExit = serializedObject.FindProperty("onHoverExit");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.PropertyField(_text);
+
+            if (_text.objectReferenceValue != null)
+            {
+                EditorGUILayout.PropertyField(_clrNormal);
+                EditorGUILayout.PropertyField(_clrHighlighted);
+                EditorGUILayout.PropertyField(_clrPressed);
+                EditorGUILayout.PropertyField(_clrDisabled);
+                EditorGUILayout.PropertyField(_clrIsOn);
+            }
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.PropertyField(_onNormal);
+            EditorGUILayout.PropertyField(_onHighlighted);
+            EditorGUILayout.PropertyField(_onPressed);
+            EditorGUILayout.PropertyField(_onDisabled);
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.PropertyField(_onHoverEnter);
+            EditorGUILayout.PropertyField(_onHoverExit);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
